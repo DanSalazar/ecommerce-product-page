f721eb296e37b6aa0eb00586d7f92b60f1002d66
Dan <danielsalazar.cc@gmail.com>
2021-10-11 18:48:19 -0400

    feat: Finish view in mobile

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav/Nav.jsx'
+import Product from './components/Product/Product.jsx'
+
+function App () {
+
+  return (
+    <div className="App">
+      <Nav/>
+      <Product/>
+    </div>
+  )
+}
+
+export default App
+
