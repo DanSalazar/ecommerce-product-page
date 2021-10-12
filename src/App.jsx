501cb3a316ad8af2f52fce2203665768fca495c6
Dan <danielsalazar.cc@gmail.com>
2021-10-11 21:12:23 -0400

    feat: Add redux store to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Nav from './components/Nav/Nav.jsx'
 import Product from './components/Product/Product.jsx'
+import store from './redux/store.js'
+import { Provider } from 'react-redux'
 
 function App () {
 
   return (
-    <div className="App">
-      <Nav/>
-      <Product/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Nav/>
+        <Product/>
+      </div>
+    </Provider>
   )
 }
 
