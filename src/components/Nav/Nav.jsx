05286c06c07567e84845c3b49086b9b6e719a267
Dan <danielsalazar.cc@gmail.com>
2021-10-11 21:09:05 -0400

    feat: Add navigation menu in mobile view

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,32 +1,50 @@
 import styles from './styles.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { showMenu, closeMenu } from '../../redux/actions/menuActions.js'
 
 const navLinks = ['Collection', 'Men', 'Women', 'About', 'Contact']
 
 function Nav () {
+	const menuState = useSelector(store => store.showMenu)
+	const dispatch = useDispatch()
+	
+	const handleShowMenu = () => {
+		if (menuState) dispatch(closeMenu())
+		else dispatch(showMenu())
+	}
+	
 	return (
 		<nav className={styles.navbar}>
 
-			<div>
-				<button className={styles.navButton}>
-					<img src="assets/icon-menu.svg" alt="Menu"/>
-				</button>
-
-				<a href='#'>
-					<img src='assets/logo.svg' alt="Company logo"/>
-				</a>
-			</div>
-
-			<div className={styles.navigation}>
-				{navLinks.map(link => (
-					<a href="#" key={link} className={styles.navLink}>{link}</a>
-				))}
+			<div className={styles.navigationContainer} >
+				<div>
+					<button className={styles.navButton} onClick={handleShowMenu}>
+						<img src="assets/icon-menu.svg" alt="Menu"/>
+					</button>
+	
+					<a href='#' className={styles.navLogo}>
+						<img src='assets/logo.svg' alt="Company logo"/>
+					</a>
+				</div>
+	
+				<div className={`${menuState && styles.navigationOverlay}`}>
+					<div className={`${styles.navigation} ${menuState && styles.showMenu}`}>
+						{menuState && <button className={styles.navButton} onClick={handleShowMenu}>
+							<img src="assets/icon-close.svg" alt="Close Menu"/>
+						</button>}
+						
+						{navLinks.map(link => (
+							<a href="#" key={link} className={styles.navLink}>{link}</a>
+						))}
+					</div>
+				</div>
 			</div>
 
 			<div className={styles.navUserStatus} >
 			  <button className={styles.cart}>
 					<img src="assets/icon-cart.svg" alt="Icon of shopping cart"/>
 				</button>
-				<img src="assets/image-avatar.png" className={styles.avatar} alt=""/>
+				<img src="assets/image-avatar.png" className={styles.avatar} alt="User"/>
 			</div>
 
 		</nav>
