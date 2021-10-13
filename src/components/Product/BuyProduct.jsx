0802049448a25aa61cbc95c8c30dc66058061f17
Dan <danielsalazar.cc@gmail.com>
2021-10-12 22:38:20 -0400

    feat: Implementing buy product

--- src/components/Product/BuyProduct.jsx
+++ src/components/Product/BuyProduct.jsx
@@ -1,32 +1,47 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import styles from './styles.module.css'
+
+import { addProduct } from '../../redux/actions/shoppingCartActions.js'
 
 import Button from '../Button/Button.jsx'
 import Cart from '../Icons/Cart.jsx'
 
 function BuyProduct () {
 	const [count, setCount] = useState(0)
+	const dispatch = useDispatch()
 
 	const handlePlus = () => {
 		setCount(count + 1)
 	}
 
 	const handleMinus = () => {
-		setCount(count - 1)
+		if (count > 0) setCount(count - 1)
+	}
+	
+	const buyProduct = () => {
+		const product = {
+			quantity: count,
+			productName: 'Fall Limited Edition Sneakers',
+			price: 125
+		}
+		
+		dispatch(addProduct(product))
+		setCount(0)
 	}
 	
 	return (
-		<div>
+		<div className={styles.buyProductContainer}>
 			<div className={styles.productCount} >
 				<button onClick={handleMinus} >
-					<img src="assets/icon-minus.svg" alt=""/>
+					<img src='assets/icon-minus.svg' alt="-"/>
 				</button>
 				<span>{count}</span>
 				<button onClick={handlePlus} >
-					<img src="assets/icon-plus.svg" alt=""/>
+					<img src='assets/icon-plus.svg' alt="+"/>
 				</button>
 			</div>
-			<Button>
+			<Button onClick={buyProduct}>
 				<Cart fill={'#fff'}/>
 				<span> Add to cart</span>
 			</Button>
--- src/components/Product/ProductInfo.jsx
+++ src/components/Product/ProductInfo.jsx
@@ -8,25 +8,25 @@ function ProductInfo () {
 
 			<span className={styles.companyName}>Sneaker Company</span>
 			
-			<h2 className={styles.productName} >Fall Limited Edition Sneakers </h2>
+			<h2 className={styles.productName}>Fall Limited Edition Sneakers</h2>
 
 			<p className={styles.productDescription} >
 				These low-profile sneakers are your perfect casual wear companion. Featuring a 
   			durable rubber outer sole, they’ll withstand everything the weather can offer.
 			</p>
 
-			<div className={styles.productPrice} >
+			<div className={styles.productPrice}>
 
 				<div className={styles.productPriceWrapper}>
 					<p className={styles.productCurrentPrice}> 
 						$125.00 
 					</p>
-					<div className={styles.productDiscount} >
+					<div className={styles.productDiscount}>
 						<span>50%</span>
 					</div>
 				</div>
 
-				<p className={styles.productOriginalPrice} >
+				<p className={styles.productOriginalPrice}>
 					$250.00
 				</p>
 
