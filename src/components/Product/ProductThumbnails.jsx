57a037676d38649f86eaa0a431e15b0cc5737323
Dan <danielsalazar.cc@gmail.com>
2021-10-14 02:16:55 -0400

    feat: Add component product thumbnails in slider

---
+++ src/components/Product/ProductThumbnails.jsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.css'
+
+const THUMBNAIL_LIST = [1, 2, 3, 4]
+
+function ProductThumbnails ({ selected, setSelected }) {
+  
+  const handleSelected = (n) => {
+    setSelected(n)
+  }
+  
+  return (
+    <div className={styles.productThumbnails}>
+      {THUMBNAIL_LIST.map(n => (
+        <button 
+          className={`${styles.productThumbnail} ${selected === n && styles.selected}`} 
+          onClick={() => handleSelected(n)}
+          key={n}>
+          <img src={`assets/image-product-${n}-thumbnail.jpg`} alt='Image of shoes'/>
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default ProductThumbnails
