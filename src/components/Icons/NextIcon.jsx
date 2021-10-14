9f5db51b30a1b826072c85dfeb045a7ad8da9221
Dan <danielsalazar.cc@gmail.com>
2021-10-14 02:15:09 -0400

    feat: Add more icons

---
+++ src/components/Icons/NextIcon.jsx
@@ -0,0 +1,9 @@
+function NextIcon (props) {
+  return (
+    <svg width={13} height={18} xmlns="http://www.w3.org/2000/svg" {...props} viewBox="-4 -2 20 18">
+      <path d="M2 1l8 8-8 8" strokeWidth={3} fill="none" />
+    </svg>
+  )
+}
+
+export default NextIcon
---
+++ src/components/Icons/PreviousIcon.jsx
@@ -0,0 +1,9 @@
+function PreviousIcon (props) {
+  return (
+    <svg width={12} height={18} xmlns="http://www.w3.org/2000/svg" {...props} viewBox='-1 -2 20 18'>
+      <path d="M11 1L3 9l8 8" strokeWidth={3} fill="none" />
+    </svg>
+  )
+}
+
+export default PreviousIcon
